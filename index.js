d1339ce45fe8bec358da689d1d3facb1770378f5
Ryan Furness <furenberger@gmail.com>
2020-07-25 17:30:54 -0500

    heroku specific changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const app = require('./server/app');
 
-const httpListenerPort = 8080;
+let port = process.env.PORT;
+if (port === null || port === '') {
+  port = 8080;
+}
 
-const httpServer = http.createServer(app).listen(httpListenerPort, () => {
-  console.log(`app is listening at localhost:${httpListenerPort}`);
+const httpServer = http.createServer(app).listen(port, () => {
+  console.log(`app is listening at localhost:${port}`);
 });
 
 process.on('SIGTERM', () => {
